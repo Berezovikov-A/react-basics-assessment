57b38771576f7959b17a1477598d61fcfe353297
ABerezovikov <alexeyberezovikov@gmail.com>
2022-12-19 07:53:00 +0600

    simplified with useRef instead of useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import React from "react";
 
 function App() {
 
-  const [input, setInput] = React.useState(0);
+  const inputRef = React.useRef(null);
 
   const reducer = (result, action) => {
     switch (action.type) {
       case 'add': 
-        return Number(result) + Number(input);
+        return Number(result) + Number(inputRef.current.value);
       case 'subtract':
-        return Number(result) - Number(input);
+        return Number(result) - Number(inputRef.current.value);
       case 'multiply': 
-        return Number(result) * Number(input);
+        return Number(result) * Number(inputRef.current.value);
       case 'divide': 
-        return Number(result) / Number(input);
+        return Number(result) / Number(inputRef.current.value);
       case 'input': 
-        setInput(0); 
+        inputRef.current.value = null; 
         return result;
       case 'result': 
         return 0;
@@ -29,29 +29,24 @@ function App() {
   
   const [result, dispath] = React.useReducer(reducer, 0)
 
-  const handleChange = e => {
-    setInput(e.target.value);
-  };
-
-
   return (
     <>
-      <div>
-        <h1>The simplest calculator</h1>
-        <h2>Result: {result}</h2>
-        <input 
-          type="text" 
-          value={input} 
-          onChange={handleChange} />
-      </div>
-      <div>
+      <h1>The simplest calculator</h1>
+      <h2>Result: {result}</h2>      
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          pattern="[0-9]"
+          type="number" 
+          ref={inputRef}
+          placeholder="Type a number" 
+           />
         <button onClick={() => dispath({type: 'add'})}>add</button>
         <button onClick={() => dispath({type: 'subtract'})}>subtract</button>
         <button onClick={() => dispath({type: 'multiply'})}>multiply</button>
         <button onClick={() => dispath({type: 'divide'})}>divide</button>
         <button onClick={() => dispath({type: 'input'})}>reset input</button>
         <button onClick={() => dispath({type: 'result'})}>reset result</button>
-      </div>
+      </form>
     </>
   );
 
